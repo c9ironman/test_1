3a63246dc755b8a6e921824bd391c9c3a8fa6b99
c9ironman <arhan.shukul@rivet.us>
2025-06-18 15:45:41 -0700

    Update calculator.cc

--- calculator.cc
+++ calculator.cc
@@ -1,26 +1,36 @@
-int num1, num2;
- double sum;
- int addType;
- cout << "Type a number: ";
- cin >> num1;
- cout << "Type a second number: ";
- cin >> num2;
- cout << "Do you want to 1. add, 2. subtract, 3. divide or 4. multiply: ";
- cin >> addType;
- if (addType == 1) {
-   sum = num1 + num2;
- }
- else if (addType == 2) {
-   sum = num1 - num2;
- }
+#include <iostream>
+using namespace std;
 
- else if (addType == 3) {
-   sum = num1 / num2;
- }
- else if (addType == 4) {
-   sum = num1 * num2;
- }
- 
- cout << "Your total is: " << sum;
- 
+int main() {
+    int num1, num2;
+    double sum;
+    int addType;
+
+    cout << "Type a number: ";
+    cin >> num1;
+    cout << "Type a second number: ";
+    cin >> num2;
+    cout << "Do you want to 1. add, 2. subtract, 3. divide or 4. multiply: ";
+    cin >> addType;
+
+    if (addType == 1) {
+        sum = num1 + num2;
+    } else if (addType == 2) {
+        sum = num1 - num2;
+    } else if (addType == 3) {
+        if (num2 == 0) {
+            cout << "Cannot divide by zero.\n";
+            return 1;
+        }
+        sum = static_cast<double>(num1) / num2;
+    } else if (addType == 4) {
+        sum = num1 * num2;
+    } else {
+        cout << "Invalid option.\n";
+        return 1;
+    }
+
+    cout << "Your total is: " << sum << endl;
+
+    return 0;
 }
